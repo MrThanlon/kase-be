0d3f28c736336f6f65d282ed35ca88ac23874c86
MrThanlon <i@anclx.cc>
2019-08-31 18:17:58 +0800

    CI: init
    Signed-off-by: MrThanlon <i@anclx.cc>

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,11 @@ COPY ./ /build/
 RUN set -xe;\
     cd /build;\
     cp nginx.conf /etc/nginx/conf.d/;\
+    php -r "copy('https://getcomposer.org/download/1.9.0/composer.phar', 'composer-setup.php');";\
+    php -r "if (hash_file('sha256', 'composer-setup.php') === 'c9dff69d092bdec14dee64df6677e7430163509798895fbd54891c166c5c0875') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;";\
+    php composer-setup.php;\
+    php -r "unlink('composer-setup.php');";\
+    composer install -vvv;\
     cd /;\
     mv /build /app;\
     mkdir /storage;\
--- build.sh
+++ build.sh
@@ -1,11 +1,3 @@
 #!/bin/sh
 
-sed -i 's/dl-cdn.alpinelinux.org/mirrors.ustc.edu.cn/g' /etc/apk/repositories
-apk update
-apk add php php-fpm
-php -r "copy('https://getcomposer.org/download/1.9.0/composer.phar', 'composer-setup.php');"
-php -r "if (hash_file('sha256', 'composer-setup.php') === 'c9dff69d092bdec14dee64df6677e7430163509798895fbd54891c166c5c0875') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-php composer-setup.php
-php -r "unlink('composer-setup.php');"
-composer install -vvv
 docker build -t $DOCKER_BUILD_TAG --build-arg "ENV=$ENV" .
