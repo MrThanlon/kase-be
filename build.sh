499c0beaa342df57510337f57691e69aad7b3c6f
MrThanlon <i@anclx.cc>
2019-08-31 18:14:50 +0800

    CI: init
    Signed-off-by: MrThanlon <i@anclx.cc>

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+sed -i 's/dl-cdn.alpinelinux.org/mirrors.ustc.edu.cn/g' /etc/apk/repositories
+apk update
+apk add php php-fpm
 php -r "copy('https://getcomposer.org/download/1.9.0/composer.phar', 'composer-setup.php');"
 php -r "if (hash_file('sha256', 'composer-setup.php') === 'c9dff69d092bdec14dee64df6677e7430163509798895fbd54891c166c5c0875') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 php composer-setup.php
