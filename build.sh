aaf972c6e03a6d6f7a754c160cd10254697949c6
MrThanlon <i@anclx.cc>
2019-11-12 22:41:32 +0800

    fix: config
    Signed-off-by: MrThanlon <i@anclx.cc>

--- build.sh
+++ build.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
-set -xe
+set -e
 sed -i 's/dl-cdn.alpinelinux.org/mirrors.ustc.edu.cn/g' /etc/apk/repositories
 apk add git
 ENV=$(git branch --show-current)
 if [ $ENV = "master" ]
 then
     $ENV="production"
+else
+    if [ $ENV = "staging" ]
+    then
+        $ENV="staging"
+    else
+        logerror "Failed, unknow branch: $ENV"
+    fi
 fi
 echo $ENV
 docker build -t $DOCKER_BUILD_TAG --build-arg "ENV=$ENV" .
